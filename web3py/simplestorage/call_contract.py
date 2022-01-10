601bca736af818ce2940d325c5ace46bad05a1b8
lbhymshr <mshr.lbhy@gmail.com>
2022-01-10 17:17:43 +0530

    Complete the contract function call python script

--- web3py/simplestorage/call_contract.py
+++ web3py/simplestorage/call_contract.py
@@ -39,29 +39,44 @@ pretty_contractABI = json.dumps(contractABI, indent=4)
 # Call -> simulate making a call and getting a return value
 # Transact -> actually make a state change on the blockchain
 
-print("We are now going to call the contract functions....")
-user_flag = True
+print("We are now going to call the contract functions. Select N when you are done")
+user_flag = 'Y'
+while (user_flag == 'Y' || user_flag == 'y'):
+    os.system('clear')
+    print("We are now going to call the contract functions. Select N when you are done")
+    print(f"There are {len(contractABI)} callable functions in the contract")
+    idx = 0
+    for fxn in contractABI:
+        print(f"Serial Number: {idx}"," || ",f"Function Name: {fxn['name']}", " || ", f"Function Type: {fxn['stateMutability']}")
+        idx +=1
+    called_fxn_idx = input("Which function would you like to call? Pick a serial number from above list: ")
 
-print(f"There are {len(contractABI)} callable functions in the contract")
-idx = 0
-for fxn in contractABI:
-    print(f"Serial Number: {idx}"," || ",f"Function Name: {fxn['name']}", " || ", f"Function Type: {fxn['stateMutability']}")
-    idx +=1
-called_fxn_idx = input("Which function would you like to call? Pick a serial number from above list: ")
+    contract_fxn = contractABI[int(called_fxn_idx)]
 
-contract_fxn = contractABI[int(called_fxn_idx)]
-print(contract_fxn)
-
-'''
-# Get the initial value of the favorite number
-print(loaded_contract.functions.retrieve().call())
-
-# Store a new value to favorite number; build a transaction, sign it and then send it 
-store_txn = loaded_contract.functions.store(15).buildTransaction({"chainId": chain_id, "gasPrice": w3.eth.gas_price, "from": my_address, "nonce": nonce})
-signed_store_txn = w3.eth.account.sign_transaction(store_txn, private_key = private_key)
-store_txn_hash = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
-store_txn_receipt = w3.eth.wait_for_transaction_receipt(store_txn_hash)
-
-# Get the new value of the favorite number
-print(loaded_contract.functions.retrieve().call())
-'''
+    # Function builder
+    contract_fxn = contractABI[int(called_fxn_idx)]
+    fxn_name = contract_fxn["name"]
+    fxn_inputs = tuple([(inputs["internalType"], inputs["name"]) for inputs in contract_fxn["inputs"]])
+    fxn_type =  contract_fxn["stateMutability"] 
+    # Argument builder
+    arguments = "("
+    for inputs in fxn_inputs:
+        arg_val = input(f"Enter the {inputs[0]} type value for {inputs[1]} (strings within '' and numbers without ''): ")
+        if arguments!="(":
+            arguments = arguments + f", {arg_val}"
+        else:
+            arguments = arguments + f"{arg_val}"
+    arguments = arguments + ")"
+    print("Initiating function call\n")
+    # Build the function statement
+    if fxn_type == "view":
+        fxn_txn = f"print(loaded_contract.functions.{fxn_name}{arguments}.call())"
+        print("Function call output:")
+        exec(fxn_txn)
+    else:
+        fxn_txn = f"store_txn = loaded_contract.functions.{fxn_name}{arguments}" + ".buildTransaction({"+ f"'chainId': {chain_id}, 'gasPrice': {w3.eth.gas_price}, 'from': '{my_address}', 'nonce': {nonce}" + "})"
+        exec(fxn_txn)
+        signed_store_txn = w3.eth.account.sign_transaction(store_txn, private_key = private_key)
+        store_txn_hash = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+        store_txn_receipt = w3.eth.wait_for_transaction_receipt(store_txn_hash)
+    user_flag = input("Contract Function has call ended. Would you like to call another function. Set N to end loop!! [Y / N]: ")
